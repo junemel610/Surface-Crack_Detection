556019981d1cd04d9826c29e4851491cd1d399c9
junemel <meljune610@gmail.com>
2023-05-24 18:32:48 +0800

    Deployment

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  #model=tf.keras.models.load_model('')
+  return model
+model=load_model()
+st.write("""
+# Surface Crack Detection System"""
+)
+file=st.file_uploader("Choose a photo from computer",type=["jpg","png"])
+A
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(120,120)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Crack', 'Without Crack']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
