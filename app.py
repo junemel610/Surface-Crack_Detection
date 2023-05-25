ff722e6c4aceca037078316d7bc6dbc0b8d39cbf
junemel <meljune610@gmail.com>
2023-05-25 22:58:21 +0800

    Surface Crack Detection Web App

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def import_and_predict(image_data, model):
     size = (120,120)
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
-    img_reshape = img[np.newaxis,...]
+    img_reshape = np.reshape(img, (1, 120, 120, 3))
     prediction = model.predict(img_reshape)
     return prediction
 if file is None:
