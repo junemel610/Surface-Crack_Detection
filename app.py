292fd71172001b7e91f259fb66729dd484547685
junemel <meljune610@gmail.com>
2023-05-25 21:58:30 +0800

    Surface Crack Detection Web App

--- app.py
+++ app.py
@@ -5,32 +5,38 @@ import numpy as np
 from PIL import Image, ImageOps
 import cv2
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
+
 def load_model_from_file():
   model=tf.keras.models.load_model('SurfaceCrackDetection.h5')
   return model
 model=load_model_from_file()
-classes = {0: 'Positive', 1: 'Negative'}
+classes = {0: 'Visible Crack/s', 1: 'No Visible Crack/s'}
 st.write("""
 # Surface Crack Detection System"""
 )
 file=st.file_uploader("Choose a photo from computer",type=["jpg","png"])
 
-def import_and_predict(image_data,model):
-    size=(120,120)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (120,120)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
     return prediction
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names= classes
-    #class_names=['Looks like there is a crack on that image you just provided',
-    #             'Looks like there is no crack on that image you just provided']
-    string="PREDICTION : "+class_names[np.argmax(prediction)]
-    st.success(string)
+    try:
+        image = Image.open(file) if file else None
+        if image:
+            st.image(image, use_column_width=True)
+            prediction = import_and_predict(image, model)
+            class_names = classes
+            string = f"Surface: {class_names[np.argmax(prediction)]}!"
+            st.success(string)
+        else:
+            st.text("The file is invalid. Upload a valid image file.")
+    except Exception as e:
+        st.text("An error occurred while processing the image.")
+        st.text(str(e))
