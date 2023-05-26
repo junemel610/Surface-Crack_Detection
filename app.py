964b39cc7700497433e51a44acccc81a61a1254e
junemel <meljune610@gmail.com>
2023-05-26 11:19:08 +0800

    Final Changes

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import tensorflow as tf
 from keras.models import load_model
 import numpy as np
 from PIL import Image, ImageOps
+import cv2
 
+st.set_page_config(page_title="Surface Crack Detection App")
+
+# Load the model
 @st.cache(allow_output_mutation=True)
 def load_model_from_file():
     model = tf.keras.models.load_model('SurfaceCrackDetection2.h5')
@@ -11,14 +15,18 @@ def load_model_from_file():
 
 model = load_model_from_file()
 
-classes = {0: 'Crack', 1: 'No Crack'}
+# Define class labels
+classes = {0: 'No Crack', 1: 'Crack'}
 
-st.write("""
-# Surface Crack Detection System
-""")
-st.write("#### Deployed by Andrea Faith Alimorong and Meljune Royette Go")
-file = st.file_uploader("Choose a photo from computer", type=["jpg", "png"])
+# Set app title and layout
+st.title("Surface Crack Detection System")
+st.markdown("---")
 
+# Create sidebar
+st.sidebar.title("Options")
+file = st.sidebar.file_uploader("Choose a photo from your computer", type=["jpg", "png"])
+
+# Process the uploaded image
 def import_and_predict(image_data, model):
     size = (120, 120)
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
@@ -27,22 +35,34 @@ def import_and_predict(image_data, model):
     prediction = model.predict(img_reshape)
     return prediction[0][0]
 
+# Main app content
 if file is None:
-    st.text("Please upload an image file")
+    st.markdown("Please upload an image file.")
 else:
     try:
         image = Image.open(file) if file else None
         if image:
             st.image(image, use_column_width=True)
+
+            # Perform prediction
             prediction = import_and_predict(image, model)
             if prediction >= 0.5:
-                result = 'Crack/s'
+                result = 'Crack'
             else:
-                result = 'no Crack/s'
-            string = f"The Surface has {result}!"
-            st.success(string)
+                result = 'No Crack'
+
+            # Display prediction result
+            st.markdown("---")
+            st.markdown(f"**Surface Condition:** {result}")
+            st.success(f"The uploaded surface image is classified as **{result}**.")
+
         else:
-            st.text("The file is invalid. Upload a valid image file.")
+            st.markdown("The file is invalid. Please upload a valid image file.")
+
     except Exception as e:
-        st.text("An error occurred while processing the image.")
-        st.text(str(e))
+        st.markdown("An error occurred while processing the image.")
+        st.markdown(f"Error Details: {str(e)}")
+
+# Add some space and footer
+st.markdown("---")
+st.markdown("Developed by [Your Name]")
